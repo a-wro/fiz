4f35d6c44ccfb4587426bbe8cc12a3cd8b350200
a-wro <34817384+a-wro@users.noreply.github.com>
2018-06-05 17:16:02 +0200

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
@@ -52,60 +51,3 @@ showBtn.grid(row=2, sticky=tk.W+tk.E, columnspan=2)
 
 
 root.mainloop()
-
-=======
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.patches import Circle
-import tkinter as tk
-
-def calcPi(e=None):
-    n = int(inp.get())
-    x = 1-2*np.random.random(int(n)) # [-1; 1]
-    y = 1-2.*np.random.random(int(n))
-    insideX,  insideY  = x[(x*x+y*y)<=1], y[(x*x+y*y)<=1]
-    print(type(insideX))
-    outsideX, outsideY = x[(x*x+y*y)>1], y[(x*x+y*y)>1]
-
-    fig, ax = plt.subplots(1)
-    ax.scatter(insideX, insideY, c='b',  s=.2, alpha=0.8, edgecolor=None)
-    ax.scatter(outsideX, outsideY, c='r', s=.2, alpha=0.8, edgecolor=None)
-
-    circle = Circle((.0, .0), 1, color='r', fill=False)
-    ax.add_patch(circle)
-    ax.set_aspect('equal')
-
-
-    inside = len(insideX)
-    pi_est = (inside / n) * 4.0
-    diff = np.pi - pi_est
-    diffLabelSv.set('Difference: ' + str(diff))
-    piLabelSv.set('Estimation: ' + str(pi_est))
-
-
-def show(e=None):
-    plt.show()
-
-root = tk.Tk()
-
-inp = tk.Entry(root, text='Number of random points')
-inp.grid(row=0, sticky=tk.W+tk.E)
-
-calculateBtn = tk.Button(root, text='Estimate pi', command=calcPi)
-calculateBtn.grid(row=0, column=1, sticky=tk.W)
-
-diffLabelSv = tk.StringVar()
-diffLabel = tk.Label(root, textvariable=diffLabelSv)
-diffLabel.grid(row=3, sticky=tk.W+tk.E, columnspan=2)
-
-piLabelSv = tk.StringVar()
-piLabel = tk.Label(root, textvariable=piLabelSv)
-piLabel.grid(row=4, sticky=tk.W+tk.E, columnspan=2)
-
-showBtn = tk.Button(root, text='Show graph', command=show)
-showBtn.grid(row=2, sticky=tk.W+tk.E, columnspan=2)
-
-
-root.mainloop()
-
->>>>>>> 3fd9b2851e979de872c7a374659097fe747b6bfe
